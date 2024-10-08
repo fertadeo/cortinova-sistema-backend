bcc4339eec27542f4930a246c957838387781950
fertadeo <fernandotadeos@gmail.com>
2024-10-08 14:07:01 -0300

    fixing configs

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,17 +4,25 @@ import { User } from '../entities/User';
 import { Clientes } from '../entities/Clientes';
 import { Pedido } from '../entities/Pedido';
 import { Producto } from '../entities/Producto';
+import dotenv from 'dotenv';
 
+// Cargar variables de entorno desde el archivo .env
+dotenv.config();
+
+// Determinar si estamos en modo producción o desarrollo
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Configurar las variables de conexión dependiendo del entorno
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '',
-  database: 'cortinova',
+  host: isProduction ? process.env.DB_HOST_PROD : process.env.DB_HOST_DEV,
+  port:  3306, // Se usa el puerto común de MySQL o uno que configures
+  username: isProduction ? process.env.DB_USER_PROD : process.env.DB_USER_DEV,
+  password: isProduction ? process.env.DB_PASSWORD_PROD : process.env.DB_PASSWORD_DEV,
+  database: isProduction ? process.env.DB_NAME_PROD : process.env.DB_NAME_DEV,
   synchronize: true,
   logging: false,
-  entities: [User, Clientes, Pedido,Producto],
+  entities: [User, Clientes, Pedido, Producto],
 });
 
 export const initializeDatabase = async () => {
