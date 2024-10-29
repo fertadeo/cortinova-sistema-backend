9889f37791341a8302200d970ae8f1e401c46e51
Fernando Tadeo <fernandotadeos@gmail.com>
2024-10-29 20:41:22 -0300

    Agregar producto individualmente

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { actualizarProducto, importarProductos, obtenerProductoPorId, obtenerTodosLosProductos } from '../controllers/productController'; // Asegúrate de que el controlador esté bien importado
+import { actualizarPreciosPorProveedor, actualizarProducto, crearProducto, importarProductos, obtenerProductoPorId, obtenerTodosLosProductos, obtenerUltimoIdProducto } from '../controllers/productController'; // Asegúrate de que el controlador esté bien importado
 
 const router = Router();
 
@@ -7,7 +7,10 @@ const router = Router();
 router.post('/importar-productos', importarProductos);
 router.get('/:id', obtenerProductoPorId);
 router.get('/', obtenerTodosLosProductos);
+router.get('/last-id/obtener', obtenerUltimoIdProducto)
 // Obtener valores de la bd luego de cargarlos con excel 
 router.get('/', obtenerProductoPorId)
 router.put('/:id', actualizarProducto); // Nueva ruta para actualizar productos
+router.put('/precios-por-proveedor/:proveedor_id', actualizarPreciosPorProveedor);
+router.post('/crear-producto', crearProducto)
 export default router; 
