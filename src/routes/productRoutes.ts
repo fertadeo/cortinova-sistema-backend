fb5fa20cb34c571c5819e4a88b2beba16ae67105
fertadeo <fernandotadeos@gmail.com>
2025-06-29 16:48:56 -0300

    Import/Export Excel products

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { actualizarPreciosPorProveedor, actualizarProducto, crearProducto, importarProductos, obtenerProductoPorId, obtenerProductosPorProveedor, obtenerRielesYBarrales, obtenerTelas, obtenerTodosLosProductos, obtenerUltimoIdProducto } from '../controllers/productController'; // Asegúrate de que el controlador esté bien importado
+import { actualizarPreciosPorProveedor, actualizarProducto, crearProducto, importarProductos, obtenerProductoPorId, obtenerProductosPorProveedor, obtenerRielesYBarrales, obtenerTelas, obtenerTodosLosProductos, obtenerUltimoIdProducto, importacionMasivaProductos, uploadMasivo, corregirPreciosCero } from '../controllers/productController'; // Asegúrate de que el controlador esté bien importado
 
 const router = Router();
 
@@ -15,4 +15,6 @@ router.put('/:id', actualizarProducto); // Nueva ruta para actualizar productos
 router.put('/actualizar-precios/:id', actualizarPreciosPorProveedor);
 router.get('/proveedor/:proveedor_id', obtenerProductosPorProveedor);
 router.post('/crear-producto', crearProducto)
+router.post('/importar-excel', importacionMasivaProductos);
+router.post('/corregir-precios-cero', corregirPreciosCero);
 export default router; 
